c83730b6e764870b6f8862c5946092baa2f4cd5f
Gorapalli Raju <129067519+GorapalliRaju@users.noreply.github.com>
2024-12-28 10:54:13 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,27 @@
-/** @type {import('tailwindcss').Config} */
 const colors = require("tailwindcss/colors");
+
+// Tailwind's default color palette includes colors that have been renamed, and if you spread over these, it will
+// trigger warnings. This code extracts the non-getter properties from the object, so that we don't trigger warnings.
+
+const entries = Object.entries(Object.getOwnPropertyDescriptors(colors)).filter(
+  ([key, descriptor]) => {
+    return typeof descriptor.get !== "function"; // Exclude getters
+  }
+);
+
+const nonDeprecatedColors = Object.fromEntries(
+  entries.map(([key, descriptor]) => [key, descriptor.value])
+);
+
 module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}", // Add paths to your project files
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}", // Add additional paths if needed
+    "./components/**/*.{js,ts,jsx,tsx,mdx}" // Add more as required
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-    colors: {
-      ...colors,
-      primary: colors.purple,
-      secondary: colors.pink,
+      colors: nonDeprecatedColors, // Use your non-deprecated color setup here
     },
   },
   plugins: [],
